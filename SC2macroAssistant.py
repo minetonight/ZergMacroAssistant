b798cd1cb99ad71502dc5221c66cc7a61767d6ec
Sam <samvel.v.matinyan@gmail.com>
2019-10-24 03:23:43 +0300

    code cleanup and documentation

--- SC2macroAssistant.py
+++ SC2macroAssistant.py
@@ -4,22 +4,37 @@
 
 from pynput.keyboard import Key, KeyCode, Listener
 import numpy as np
-# from numpy_ringbuffer import RingBuffer 
 # https://pypi.org/project/numpy_ringbuffer/ 
 # doc https://github.com/eric-wieser/numpy_ringbuffer/blob/master/numpy_ringbuffer/__init__.py
 
 import time
 from timeloop import Timeloop # https://medium.com/greedygame-engineering/an-elegant-way-to-run-periodic-tasks-in-python-61b7c477b679
 from datetime import timedelta
-scriptStart = time.time() # https://www.programiz.com/python-programming/time
-lastMacroCycle = scriptStart
-lastLarvaSpent = scriptStart
 
 from playsound import playsound
 
 
-# lastActionsBuffer = RingBuffer(capacity=3, dtype=Key) # deprecated
+'''
+I use the core 2.0 so my hotkeys are as follows:
 
+ u - inject queen hotkey (use 5 on standard layout)
+ - - inject ability for queen (use V on standard layout)
+ o - all hatcheries (use 4 on standard layout)
+ p - select all larva button (use S on standard layout)
+ p - lings hotkey (use Z on standard layout)
+ ; - roach hotkey (use R on standard layout)
+ [ - hydra hotkey (use H on standard layout)
+ h - corruptor hotkey (use C on standard layout)
+'''
+
+# cmd.exe /K "cd C:\Users\Teacher\Documents\StarCraft II\MacroAssistant == start shell in specific folder
+# playsound('macroCycle.mp3') # https://youtu.be/f0chGt6IVBo?t=2964 49:24
+# playsound('spendLarva.mp3') # https://youtu.be/O3aGlfvQiqo?t=217 3:37
+
+
+scriptStart = time.time() # https://www.programiz.com/python-programming/time
+lastMacroCycle = scriptStart
+lastLarvaSpent = scriptStart
 
 counter = 0
 bufferSize = 3
@@ -38,35 +53,32 @@ def checkMacro():
     
     
 
-# cmd /K "cd C:\Windows\"
-# playsound('macroCycle.mp3')
-# playsound('spendLarva.mp3')
-
 tl.start(block=False) # do not move this line
 
 def checkPlayerActions(lastActionIndex):
     global lastMacroCycle, lastLarvaSpent
     
+    
     if lastActionsBuffer[(lastActionIndex+1)%bufferSize]             == KeyCode.from_char('u') \
     and lastActionsBuffer[(lastActionIndex+2)%bufferSize] == KeyCode.from_char('-') \
     and lastActionsBuffer[(lastActionIndex+0)%bufferSize] == Key.shift_r:
         lastMacroCycle = time.time()
-        print ("yay, an inject was detected! lastActionIndex="+str(lastActionIndex))
-        # playsound('macroCycle.mp3')
+        print ("yay, an inject was detected!")
 
     if lastActionsBuffer[(lastActionIndex+1)%bufferSize]             == KeyCode.from_char('o') \
     and lastActionsBuffer[(lastActionIndex+2)%bufferSize] == KeyCode.from_char('p') \
     and lastActionsBuffer[(lastActionIndex+0)%bufferSize] == KeyCode.from_char('j'): # opj = drones
         lastLarvaSpent= time.time()
-        print ("yay, drones ware made! lastActionIndex="+str(lastActionIndex))
-        # playsound('spendLarva.mp3')
+        print ("yay, drones ware made!")
 
     if lastActionsBuffer[(lastActionIndex+1)%bufferSize]             == KeyCode.from_char('o') \
     and lastActionsBuffer[(lastActionIndex+2)%bufferSize] == KeyCode.from_char('p') \
-    and lastActionsBuffer[(lastActionIndex+0)%bufferSize] == KeyCode.from_char('p'): # opp = lings
+    and (lastActionsBuffer[(lastActionIndex+0)%bufferSize] == KeyCode.from_char('p')  # opp = lings
+    or lastActionsBuffer[(lastActionIndex+0)%bufferSize] == KeyCode.from_char(';')  # op; = roach
+    or lastActionsBuffer[(lastActionIndex+0)%bufferSize] == KeyCode.from_char('p')  # op[ = hydra
+    or lastActionsBuffer[(lastActionIndex+0)%bufferSize] == KeyCode.from_char('h') ): # oph = corruptor
         lastLarvaSpent = time.time()
-        print ("yay, lings were made! lastActionIndex="+str(lastActionIndex))
-        # playsound('spendLarva.mp3')
+        print ("yay, army wes made!")
 
 
 def keypress(key):
