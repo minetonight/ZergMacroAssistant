bdc52d07d214963ba84d8480ecfa8dbaf7b6d50b
Sam <samvel.v.matinyan@gmail.com>
2019-10-28 23:35:02 +0200

    added csv stats logging after each game

--- SC2macroAssistant.py
+++ SC2macroAssistant.py
@@ -13,6 +13,7 @@ from datetime import timedelta
 
 from playsound import playsound
 
+import logging
 
 '''
 I use the core 2.0 so my hotkeys are as follows:
@@ -28,29 +29,29 @@ I use the core 2.0 so my hotkeys are as follows:
 '''
 
 # cmd.exe /K "cd C:\Users\Teacher\Documents\StarCraft II\MacroAssistant == start shell in specific folder
-# playsound('macroCycle.mp3') # https://youtu.be/f0chGt6IVBo?t=2964 49:24
-# playsound('spendLarva.mp3') # https://youtu.be/O3aGlfvQiqo?t=217 3:37
 
 
 scriptStart = time.time() # https://www.programiz.com/python-programming/time
 lastMacroCycle = scriptStart
 lastLarvaSpent = scriptStart
+counter = 0
+larvaHints = 0 # count hints to track players improvement.
+larvaHints = 0
+injectHints = 0
+is_in_game = False
 
 bufferSize = 3
 lastActionsBuffer = []
 tl = Timeloop()
 
-counter = 0
-is_in_game = False
-larvaHints = 0 # count hints to track players improvement.
-injectHints = 0
 
-# TODO 3 - count hints and store them in an ever growing file, to track players improvement.
-# TODO 3: in more columns: for larva and for injects!
-# Csv:Datetime, gameDurationSeconds, gameDurationMinutes, larvaHints, injectHints, keysCount, larvaHPM, injectHPM, KPM
-# Logger basicCofigdatefmt="%d.%m.%Y %H:%M" 
-# Use the specified date/time format, as accepted by time.strftime()
-
+# setup logger for this program only, as timeloop tends to spam in the logs
+formatter = logging.Formatter(fmt='%(asctime)s, %(message)s', datefmt="%d.%m.%Y %H:%M")
+handler = logging.FileHandler("AI_Coaching.csv")        
+handler.setFormatter(formatter)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
 
 
 @tl.job(interval=timedelta(seconds=1))
@@ -63,33 +64,54 @@ def checkMacro():
         if lastMacroCycle+30 <= now: # lastCycle=30 // now=45 // now = 61
             # count hints to track players improvement.
             injectHints = injectHints + 1 # TODO dont count the same hint too many times, wait for the player to execute it before you count again
-            playsound('macroCycle.mp3')
-        
+            playsound('macroCycle.mp3') # https://youtu.be/f0chGt6IVBo?t=2964 49:24
+            
         if lastLarvaSpent+15 <= now: # lastlarva=30 // now=45 // now = 61
             # count hints to track players improvement.
             larvaHints = larvaHints + 1 # TODO dont count the same hint too many times, wait for the player to execute it before you count again
-            playsound('spendLarva.mp3')
+            playsound('spendLarva.mp3') # https://youtu.be/O3aGlfvQiqo?t=217 3:37
     
     
 tl.start(block=False) # do not move this line
 
+def storeStatsInFile():
+    # count hints and store them in an ever growing file, to track players improvement
+    # in many columns: for larva and for injects!
+    
+    global counter, larvaHints, injectHints, scriptStart
+    # Csv: Datetime, gameDurationSeconds, gameDurationMinutes, larvaHints, injectHints, keysCount, larvaHPM, injectHPM, KPM
+    scriptEnd = time.time()
+    gameDurationSeconds = scriptEnd - scriptStart
+    gameDurationMinutes = "%d:%d" % (gameDurationSeconds/60, gameDurationSeconds%60)
+    keysCount = counter
+    larvaHPM = (larvaHints/gameDurationSeconds) * 60
+    injectHPM = (injectHints/gameDurationSeconds) * 60
+    KPM = (counter/gameDurationSeconds) * 60
+    
+    csvLine = "%d, %s, %d, %d, %d, %d, %d, %d" % (gameDurationSeconds, gameDurationMinutes, larvaHints, injectHints, keysCount, larvaHPM, injectHPM, KPM)
+    print("csvLine = " + csvLine)
+    logger.info(csvLine)
+
 def checkPlayerActions(lastActionIndex):
     global lastMacroCycle, lastLarvaSpent
-    global counter, is_in_game, larvaHints, injectHints
+    global counter, is_in_game, larvaHints, injectHints, scriptStart
     
     # check for shift+insert = my inital GLHF thing, to unpause the script
     if (lastActionsBuffer[(lastActionIndex-1)%bufferSize] == Key.shift_r \
     and lastActionsBuffer[(lastActionIndex+0)%bufferSize] == Key.insert): 
-        lastMacroCycle = time.time()
-        lastLarvaSpent = time.time()
+        
+        scriptStart = time.time()
+        lastMacroCycle = scriptStart
+        lastLarvaSpent = scriptStart
         counter = 0
-        is_in_game = True
+        larvaHints = 0 
         larvaHints = 0
         injectHints = 0
-        print ("GL HF to you, too!")
+        
+        is_in_game = True
+        print("GL HF to you, too!")
         playsound("diamond x2.mp3")
         
-    
     # check for F10+n or F10+w or F10+s to stop the script
     if (lastActionsBuffer[(lastActionIndex-1)%bufferSize] == Key.f10 \
     and lastActionsBuffer[(lastActionIndex+0)%bufferSize] ==KeyCode.from_char('n')) \
@@ -98,10 +120,8 @@ def checkPlayerActions(lastActionIndex):
     or (lastActionsBuffer[(lastActionIndex-1)%bufferSize] == Key.f10 \
     and lastActionsBuffer[(lastActionIndex+0)%bufferSize] ==KeyCode.from_char('s')): 
         is_in_game = False
-        print ("Geeee Geeee!")
-        # TODO 2 - count hints and store them in an ever growing file, to track players improvement.
-        print("injectHints = " + str(injectHints))
-        print("larvaHints = " + str(larvaHints))
+        print("Geeee Geeee!")
+        storeStatsInFile() # count hints and store them in an ever growing file, to track players improvement.
         playsound("gg.mp3")
     
     # spam at the start of the game
@@ -113,7 +133,7 @@ def checkPlayerActions(lastActionIndex):
     and lastActionsBuffer[(lastActionIndex+0)%bufferSize] == KeyCode.from_char('8')): 
         lastMacroCycle = time.time()
         lastLarvaSpent= time.time()
-        print ("So, you are spamming now?")
+        print("So, you are spamming now?")
     
     # normal cycle or inital hotkey of new queen and inject 
     if (lastActionsBuffer[(lastActionIndex+1)%bufferSize] == KeyCode.from_char('u') \
@@ -123,13 +143,13 @@ def checkPlayerActions(lastActionIndex):
     and lastActionsBuffer[(lastActionIndex+2)%bufferSize] == KeyCode.from_char('u') \
     and lastActionsBuffer[(lastActionIndex+0)%bufferSize] == KeyCode.from_char('-')): # inital hotkey of new queen and inject 
         lastMacroCycle = time.time()
-        print ("yay, an inject was detected!")
+        print("yay, an inject was detected!")
 
     if lastActionsBuffer[(lastActionIndex+1)%bufferSize]  == KeyCode.from_char('o') \
     and lastActionsBuffer[(lastActionIndex+2)%bufferSize] == KeyCode.from_char('p') \
     and lastActionsBuffer[(lastActionIndex+0)%bufferSize] == KeyCode.from_char('j'): # opj = drones
         lastLarvaSpent= time.time()
-        print ("yay, drones were made!")
+        print("yay, drones were made!")
 
     if lastActionsBuffer[(lastActionIndex+1)%bufferSize]  == KeyCode.from_char('o') \
     and lastActionsBuffer[(lastActionIndex+2)%bufferSize] == KeyCode.from_char('p') \
@@ -139,7 +159,7 @@ def checkPlayerActions(lastActionIndex):
     or lastActionsBuffer[(lastActionIndex+0)%bufferSize] == KeyCode.from_char('h')  # oph = corruptor
     or lastActionsBuffer[(lastActionIndex+0)%bufferSize] == KeyCode.from_char('-') ): # op- = overlord
         lastLarvaSpent = time.time()
-        print ("yay, army was made!")
+        print("yay, army was made!")
 
 
 def keypress(key):
